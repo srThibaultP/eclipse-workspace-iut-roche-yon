f1f89abde66806157237405591fa829b5418619d
Thibault Pech <thibaultpech@outlook.com>
2021-09-28 19:27:06 +0200

    Initial commit

---
+++ 01-HelloWorld/src/main_HelloWorld.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class main_HelloWorld {
+
+	// Méthode principale de l'application
+	public static void main(String[] args) {
+			// Afficher dans la console
+			System.out.println("Hello la LP DIP !!!");
+			
+			// idem ds fenêtre
+			JOptionPane.showMessageDialog(null, "En direct de LR");
+		}
+
+	}
---
+++ 13-Tableaux/src/mainTableaux.java
@@ -0,0 +1,7 @@
+
+public class mainTableaux {
+	public static void main(String[] args) {
+		double [] devises = {1.17, 130.04, 0.86, 7.54};
+		
+	}
+}
