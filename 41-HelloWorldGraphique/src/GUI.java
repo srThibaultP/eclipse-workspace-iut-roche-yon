5a604ad647da0190f065e03635c0147b6067c56b
Thibault Pech <thibaultpech@outlook.com>
2021-10-11 14:49:47 +0200

    Début TP Gui

---
+++ 41-HelloWorldGraphique/src/GUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+    private Panneau fond;
+    private Jlabel affTexte;
+    private BorderLayer structure;
+}
---
+++ 41-HelloWorldGraphique/src/Panneau.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panneau {
+    private Image Image;
+
+    public Panneau(Image pImage) {}
+    public void paintComponent(Graphics crayon) {}
+}
