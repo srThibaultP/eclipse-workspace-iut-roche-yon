e369a8d8e7e1ef35dd4e028d7359697f930cd83e
ThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2021-11-09 23:49:18 +0100

    opti ascii

--- 43-Cesar/src/lry/dip/cesar/Cesar.java
+++ 43-Cesar/src/lry/dip/cesar/Cesar.java
@@ -2,7 +2,6 @@ package lry.dip.cesar;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.Reader;
 import java.io.Writer;
 
 import javax.swing.JFileChooser;
@@ -25,79 +24,42 @@ public class Cesar {
 
 	public void Coder_Cesar(String motAcoder){
 		this.messageCode = "";
-		this.messageDecode = motAcoder;
-		for(int i = 0; i < this.messageDecode.length(); i++){
-			char c = this.messageDecode.charAt(i);
-			int temp;
-			if(Character.isLetter(c)){
-				if(Character.isUpperCase(c)){
-					int ascii = (int) c;
-					ascii += key;
-					if (ascii > 90) {
-						temp = ascii - 91;
-						ascii = 65 + temp;
-					}
-					c = (char) ascii;
-				}else{
-					int ascii = (int) c;
-					ascii += key;
-					if (ascii > 122) {
-						temp = ascii - 123;
-						ascii = 97 + temp;
-					}
-					c = (char) ascii;
+		int ascii = 0;
+
+		for(int i = 0; i < motAcoder.length(); i++){
+			ascii = motAcoder.charAt(i);
+				if ((ascii >= 97 && ascii <= 122 - this.key) || (ascii >= 65 && ascii <= 90 - this.key)) {
+					ascii += this.key;
+				} else if ((ascii >= 122 - this.key && ascii <= 122) || (ascii >= 90 - this.key && ascii <= 90)) {
+					ascii = ascii - 26 + this.key;
 				}
-			}
-			this.messageCode += c;
+				this.messageCode += (char) ascii;
 		}
 	}
 	
-	public void Decoder_Cesar(String motAdecoder){
+	public void Decoder_Cesar(String motAdecoder) {
 		this.messageDecode = "";
-		this.messageCode = motAdecoder;
-		for(int i = 0; i < this.messageCode.length(); i++){
-			char c = this.messageCode.charAt(i);
-			int temp = 0;
-			if(Character.isLetter(c)){
-				if(Character.isUpperCase(c)){
-					int ascii = (int) c;
-					ascii -= key;
-					if (ascii < 64) {
-						temp = 64 - ascii;
-						ascii = 90 - temp;
-					}
-					c = (char) ascii;
-				}else{//LOWERCASE
-					int ascii = (int) c;
-					ascii -= key;
-					if (ascii < 96) {
-						temp = 96 - ascii;
-						ascii = 122 - temp;
-					}
-					c = (char) ascii;
-				}
+		int ascii = 0;
+		for (int i = 0; i < motAdecoder.length(); i++) {
+			ascii = motAdecoder.charAt(i);
+			if ((ascii >= 97 + this.key && ascii <= 122) || (ascii >= 65 + this.key && ascii <= 90)) {
+				ascii -= this.key;
+			} else if ((ascii >= 97 && ascii <= 97 + this.key) || (ascii >= 65 && ascii <= 65 + this.key)) {
+				ascii = ascii - this.key + 26;
 			}
-			this.messageDecode += c;
+			this.messageDecode += (char) ascii;
 		}
 	}
-/*
-	public void enrFichier(String cheminFichier, String texte) {
-		try {
-			Writer fichier = new java.io.FileWriter(cheminFichier);
-			fichier.write(texte);
-			fichier.close();
-		} catch (Exception e) {
-			System.out.println("Erreur d'enregistrement du fichier : " + e);
-		}
-	}
-*/
+
 	public void enrFichier(GUI cheminFichier) {
 		if (this.selectionFichier.showOpenDialog(cheminFichier) == 0) {
 			this.fichier = this.selectionFichier.getSelectedFile();
+
 			try {
 				Writer ecriture = new java.io.FileWriter(this.fichier);
 				ecriture.write(this.messageCode);
 				ecriture.close();
+
 			} catch (Exception e) {
 				System.out.println("Erreur d'enregistrement du fichier : " + e);
 			}
@@ -106,15 +68,19 @@ public class Cesar {
 	public void lireFichier(GUI cheminFichier) {
 		this.messageCode = "";
 		if (this.selectionFichier.showOpenDialog(cheminFichier) == 0) {
+
 			try {
 				this.fichierLecture = new FileReader(new File(this.selectionFichier.getSelectedFile().getAbsolutePath()));
 				this.cacheFichier = new BufferedReader(this.fichierLecture);
 				String lettre = new String();
+				
 				while ((lettre = this.cacheFichier.readLine()) != null) {
 					this.messageCode = String.valueOf(this.messageCode) + lettre;
 				}
+
 				this.cacheFichier.close();
 				this.fichierLecture.close();
+
 			} catch (Exception e) {
 				System.out.println("Erreur de lecture du fichier : " + e);
 			}
@@ -122,39 +88,28 @@ public class Cesar {
 	}
 
 
-
-/*
-	public void lireFichier(String cheminFichier) {
-		try {
-			Reader fichier = new java.io.FileReader(cheminFichier);
-			int c;
-			String txt = "";
-			while ((c = fichier.read()) != -1) {
-				txt += (char) c;
-			}
-			Decoder_Cesar(txt);
-			fichier.close();
-		} catch (Exception e) {
-			System.out.println("Erreur de lecture du fichier : " + e);
-		}
-	}
-*/
 	public String getMessageDecode() {
 		return messageDecode;
 	}
+
 	public void setMessageDecode(String pMessage) {
 		this.messageDecode = pMessage;
 	}
+
 	public String getMessageCode() {
 		return messageCode;
 	}
+
 	public void setMessageCode(String pMessage) {
 		this.messageCode = pMessage;
 	}
+
 	public int getKey() {
 		return key;
 	}
+
 	public void setKey(int pKey) {
 		this.key = pKey;
 	}
+
 }
